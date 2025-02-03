f23339b8819df703b6a5acbc04b2e1626b957db1
daraun white <daraun@MacBookPro.lan>
2025-02-02 21:25:32 -0500

    randomized data

--- read_clean.R
+++ read_clean.R
@@ -2,33 +2,39 @@
 
 ## Change the working directory and file paths as appropriate
 
-setwd("~/Dropbox/Projects/Senscio/DSC480_sp_2025")
+setwd("/Users/daraun/DSC480")
 
 library(tidyverse)
+library(dplyr)
 
 # Actual data files
 #events <- read_csv("../data/2024_11_25_utilization_events.csv")
 #report <- read_csv("../data/2024_11_25_utilization_report.csv")
 
 # Toy data files- these have the row values for each column shuffled separately.
-events <- read_csv("../data/events_toy.csv")
-report <- read_csv("../data/report_toy.csv")
+events <- read_csv("events_toy.csv")
+report <- read_csv("report_toy.csv")
 
 
 ## A bit of cleaning
-events_full <- events %>%
-  mutate_at(vars(contains("timestamp")), ~as.POSIXct(.x, format = "%Y-%m-%d %H:%M:%S")) %>%
-  mutate_at(vars(contains("timestamp")), ~as_date(.x)) %>%
+reports_full <- report %>%
+  mutate(across(contains("timestamp"), ~as.POSIXct(.x, format = "%Y-%m-%d %H:%M:%S"))) %>%
+  mutate(across(contains("timestamp"), ~as_date(.x))) %>%
   rename_with(~str_replace(., "timestamp", "date"), contains("timestamp")) %>%
   mutate(across(event_type, ~ str_replace(.x, " ", "_"))) |>
   mutate(across(event_type, ~as.factor(.x)))
+glimpse(reports_full)
 
-
-report_full <- report %>%
-  mutate_at(vars(contains("timestamp")), ~as.POSIXct(.x, format = "%Y-%m-%d %H:%M:%S")) %>%
-  mutate_at(vars(contains("timestamp")), ~as_date(.x)) %>%
+events_full <- events %>%
+  mutate(across(contains("timestamp"), ~as.POSIXct(.x, format = "%Y-%m-%d %H:%M:%S"))) %>%
+  mutate(across(contains("timestamp"), ~as_date(.x))) %>%
   rename_with(~str_replace(., "timestamp", "date"), contains("timestamp")) %>%
-    mutate(across(event_type, ~ str_replace(.x, " ", "_"))) |>
+  mutate(across(event_type, ~ str_replace(.x, " ", "_"))) |>
   mutate(across(event_type, ~as.factor(.x)))
+glimpse(events_full)
 
-## add comment
+library(dplyr)
+library(lubridate)
+
+
+
